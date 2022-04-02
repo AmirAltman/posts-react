56f7fa149c9778c2efdc0bae8a3493fcc6abb690
Amir Altman <amiral@nvidia.com>
2022-04-02 12:00:23 +0300

    added full stats page that shows topCreators and runTimes avgs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import PostDashboard from "./pages/PostDashboard";
 import StatsDashboard from "./pages/StatsDashboard";
--- src/pages/StatsDashboard.js
+++ src/pages/StatsDashboard.js
@@ -1,11 +1,37 @@
 import React from "react";
+import {useQuery} from 'react-query'
+import Loader from "../components/Loader";
+import {getter} from "../utils";
+import {Link} from "react-router-dom";
 
 const StatsDashboard = () => {
+    const {status: creatorStatus, data: topCreators, error: CreatorError} = useQuery('topCreators', getter('topCreators', 'topCreators'),
+        {refetchOnWindowFocus: false})
+    const {status: runTimeStatus, data: runTimes, error: runTimeError} = useQuery('avgRunTime', getter('avgRunTime', 'runTimes'),
+        {refetchOnWindowFocus: false})
+
+    if (creatorStatus === 'loading' || runTimeStatus === "loading") {
+        return <Loader/>
+    }
+    if (CreatorError === 'error' || runTimeError === "error") {
+        return <span>Error occurred</span>
+    }
 
 
     return (
         <div className="post-stats-container">
-            stats page
+        <span>
+            the top creators are {topCreators.map(name => <li key={name}>{name}</li>)}
+        </span>
+            <span>
+            The avg run time are
+                {
+                    runTimes.map(runTimeData =>
+                        <li key={runTimeData._id}>  {runTimeData._id} is {runTimeData.avgRunTime}</li>
+                    )
+                }
+        </span>
+            <Link to="/">Posts</Link>
         </div>
     )
 
